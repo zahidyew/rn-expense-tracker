43d48ba85d5c4945920a538f1ca174ee3e53ac53
zahidyew <47619501+zahidyew@users.noreply.github.com>
2021-05-26 15:52:16 +0800

    Add redux and async-storage packages to project

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
