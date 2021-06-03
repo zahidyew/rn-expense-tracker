54a7490519a87f7ef89cab79fdc42f46668388fc
zahidyew <47619501+zahidyew@users.noreply.github.com>
2021-06-03 22:37:22 +0800

    Update getDate() implementation to return Date in diff. formats

--- src/helpers/Dates.ts
+++ src/helpers/Dates.ts
@@ -1,7 +1,35 @@
-export const getDate = () => {
+type DateFormat =
+  | 'weekday'
+  | 'day'
+  | 'month'
+  | 'year'
+  | 'dayMonth'
+  | 'monthYear'
+  | 'dayMonthYear';
+
+export const getDate = (format: DateFormat) => {
   const [month, day, year] = new Date().toLocaleDateString('en-US').split('/');
   //const [hour, minute] = new Date().toLocaleTimeString('en-US').split(/:| /);
-  const date = `${day}/${month}`;
 
-  return date;
+  switch (format) {
+    case 'weekday':
+      return new Date().toLocaleDateString('en-US', { weekday: 'short' });
+    case 'day':
+      return new Date().toLocaleDateString('en-US', { day: 'numeric' });
+    case 'month':
+      return new Date().toLocaleDateString('en-US', { month: 'long' });
+    case 'year':
+      return year;
+    case 'dayMonth':
+      return `${day}/${month}`;
+    case 'monthYear':
+      return `${month}/${year}`;
+    case 'dayMonthYear':
+      return `${day}/${month}/${year}`;
+    default:
+      console.error(
+        'Incorrect date format passed in function getDate() in Dates.ts',
+      );
+      return `${day}/${month}/${year}`;
+  }
 };
