e10ccb1a64ddbcd3a4eca2e1603dfc71e1e74ffb
zahidyew <47619501+zahidyew@users.noreply.github.com>
2021-06-16 17:19:24 +0800

    Update test for filteringExpense

--- __tests__/helpers/Filters.ts
+++ __tests__/helpers/Filters.ts
@@ -6,34 +6,35 @@ test('filtering expenses', () => {
       id: 1,
       name: 'Coffee',
       price: 5,
-      date: '14/6/2021',
+      date: '14 Jun 2021',
     },
     {
       id: 2,
       name: 'Tea',
       price: 3,
-      date: '2/9/2021',
+      date: '2 Sep 2021',
     },
   ];
 
-  expect(filterExpenses(expense, '9', '2021')).not.toEqual([]);
-  expect(filterExpenses(expense, '9', '2021')).toEqual([
+  expect(filterExpenses(expense, 'Sep', '2021')).not.toEqual([]);
+  expect(filterExpenses(expense, 'Sep', '2021')).toEqual([
     {
       id: 2,
       name: 'Tea',
       price: 3,
-      date: '2/9/2021',
+      date: '2 Sep 2021',
     },
   ]);
-  expect(filterExpenses(expense, '6', '2021')).not.toEqual([]);
-  expect(filterExpenses(expense, '6', '2021')).toEqual([
+  expect(filterExpenses(expense, 'Jun', '2021')).not.toEqual([]);
+  expect(filterExpenses(expense, 'Jun', '2021')).toEqual([
     {
       id: 1,
       name: 'Coffee',
       price: 5,
-      date: '14/6/2021',
+      date: '14 Jun 2021',
     },
   ]);
   expect(filterExpenses(expense, '5', '2021')).toEqual([]);
-  expect(filterExpenses(expense, '6', '2020')).toEqual([]);
+  expect(filterExpenses(expense, 'June', '2020')).toEqual([]);
+  expect(filterExpenses(expense, 'Jun', '2020')).toEqual([]);
 });
