a8d57627fb028bd4344dcc616c5b65e1656eeb93
zahidyew <47619501+zahidyew@users.noreply.github.com>
2021-06-21 19:09:07 +0800

    Add delete button on ExpenseScreen when editing

--- src/containers/ExpenseScreen.tsx
+++ src/containers/ExpenseScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import { createBox } from '@shopify/restyle';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { createBox, createText } from '@shopify/restyle';
 import { Theme } from '@styles/restyle';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { HomeStackParamList } from '@src/App';
 import { RouteProp } from '@react-navigation/native';
 import CategoriesIcons from '@src/components/CategoriesIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export type ExpenseScreenNavigationProp = StackNavigationProp<
   HomeStackParamList,
@@ -23,11 +24,19 @@ type Props = {
 };
 
 const Box = createBox<Theme>();
+const Text = createText<Theme>();
 
 const ExpenseScreen = ({ navigation, route }: Props) => {
   useEffect(() => {
     if (route.params.isEditing) {
-      navigation.setOptions({ title: 'Update Expense' });
+      navigation.setOptions({
+        title: 'Update Expense',
+        headerRight: () => (
+          <TouchableOpacity style={{ paddingRight: 8 }} onPress={() => {}}>
+            <Ionicons name="trash" size={24} color="red" />
+          </TouchableOpacity>
+        ),
+      });
     } else {
       navigation.setOptions({ title: 'New Expense' });
     }
