44b7f8de7d81d30be84c80a3430354779b9ec676
zahidyew <47619501+zahidyew@users.noreply.github.com>
2021-06-30 15:01:43 +0800

    Create a new model Category

--- src/components/CategoriesIcons.tsx
+++ src/components/CategoriesIcons.tsx
@@ -1,4 +1,5 @@
 import { createBox, createText, useTheme } from '@shopify/restyle';
+import { categories, Category } from '@src/models/Category';
 import { Theme } from '@src/styles/restyle';
 import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, StyleSheet } from 'react-native';
@@ -7,17 +8,16 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 const Box = createBox<Theme>();
 const Text = createText<Theme>();
 
-export interface Category {
-  id: number;
-  name: string;
-  icon: string;
-}
-
 // need to make sure each category name is unique
 interface Props {
   itemName?: string;
   onClick: (openNumpad: boolean, categoryName: string) => void;
 }
+
+const categoriesToDisplay = [
+  ...categories,
+  { id: 99, name: 'Add', icon: 'add-outline' },
+];
 
 const CategoriesIcons = (props: Props) => {
   const theme = useTheme<Theme>();
@@ -29,104 +29,6 @@ const CategoriesIcons = (props: Props) => {
   useEffect(() => {
     setSelectedCategory(itemName ? itemName : '');
   }, [itemName]);
-
-  const categories: Category[] = [
-    {
-      id: 1,
-      name: 'Food',
-      icon: 'fast-food-outline',
-    },
-    {
-      id: 2,
-      name: 'Transportation',
-      icon: 'bus-outline',
-    },
-    {
-      id: 3,
-      name: 'Home',
-      icon: 'home-outline',
-    },
-    {
-      id: 4,
-      name: 'Car',
-      icon: 'car-sport-outline',
-    },
-    {
-      id: 5,
-      name: 'Beverages',
-      icon: 'wine-outline',
-    },
-    {
-      id: 6,
-      name: 'Clothing',
-      icon: 'shirt-outline',
-    },
-    {
-      id: 7,
-      name: 'Telco',
-      icon: 'call-outline',
-    },
-    {
-      id: 8,
-      name: 'Entertainment',
-      icon: 'game-controller-outline',
-    },
-    {
-      id: 9,
-      name: 'Shopping',
-      icon: 'card-outline',
-    },
-    {
-      id: 10,
-      name: 'Health',
-      icon: 'medkit-outline',
-    },
-    {
-      id: 11,
-      name: 'Electronic',
-      icon: 'desktop-outline',
-    },
-    {
-      id: 12,
-      name: 'Snacks',
-      icon: 'ice-cream-outline',
-    },
-    {
-      id: 13,
-      name: 'Gift',
-      icon: 'gift-outline',
-    },
-    {
-      id: 14,
-      name: 'Office',
-      icon: 'business-outline',
-    },
-    {
-      id: 15,
-      name: 'Groceries',
-      icon: 'cart-outline',
-    },
-    {
-      id: 16,
-      name: 'Toiletries',
-      icon: 'water-outline',
-    },
-    {
-      id: 17,
-      name: 'Petrol',
-      icon: 'car-outline',
-    },
-    {
-      id: 18,
-      name: 'Others',
-      icon: 'receipt-outline',
-    },
-  ];
-
-  const categoriesToDisplay = [
-    ...categories,
-    { id: 99, name: 'Add', icon: 'add-outline' },
-  ];
 
   const getNumberOfRows = (categoriesLength: number) => {
     const rows = Math.ceil(categoriesLength / 4);
---
+++ src/models/Category.ts
@@ -0,0 +1,98 @@
+export interface Category {
+  id: number;
+  name: string;
+  icon: string;
+}
+
+export const categories: Category[] = [
+  {
+    id: 1,
+    name: 'Food',
+    icon: 'fast-food-outline',
+  },
+  {
+    id: 2,
+    name: 'Transportation',
+    icon: 'bus-outline',
+  },
+  {
+    id: 3,
+    name: 'Home',
+    icon: 'home-outline',
+  },
+  {
+    id: 4,
+    name: 'Car',
+    icon: 'car-sport-outline',
+  },
+  {
+    id: 5,
+    name: 'Beverages',
+    icon: 'wine-outline',
+  },
+  {
+    id: 6,
+    name: 'Clothing',
+    icon: 'shirt-outline',
+  },
+  {
+    id: 7,
+    name: 'Telco',
+    icon: 'call-outline',
+  },
+  {
+    id: 8,
+    name: 'Entertainment',
+    icon: 'game-controller-outline',
+  },
+  {
+    id: 9,
+    name: 'Shopping',
+    icon: 'card-outline',
+  },
+  {
+    id: 10,
+    name: 'Health',
+    icon: 'medkit-outline',
+  },
+  {
+    id: 11,
+    name: 'Electronic',
+    icon: 'desktop-outline',
+  },
+  {
+    id: 12,
+    name: 'Snacks',
+    icon: 'ice-cream-outline',
+  },
+  {
+    id: 13,
+    name: 'Gift',
+    icon: 'gift-outline',
+  },
+  {
+    id: 14,
+    name: 'Office',
+    icon: 'business-outline',
+  },
+  {
+    id: 15,
+    name: 'Groceries',
+    icon: 'cart-outline',
+  },
+  {
+    id: 16,
+    name: 'Toiletries',
+    icon: 'water-outline',
+  },
+  {
+    id: 17,
+    name: 'Petrol',
+    icon: 'car-outline',
+  },
+  {
+    id: 18,
+    name: 'Others',
+    icon: 'receipt-outline',
+  },
+];
